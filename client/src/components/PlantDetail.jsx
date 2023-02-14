ce2e5610f2228173a91505032f2cececa92b8ad2
John Jacobs <john.a.jacobs1973@gmail.com>
2023-02-14 15:42:12 -0500

    Outline text

--- client/src/components/PlantDetail.jsx
+++ client/src/components/PlantDetail.jsx
@@ -9,8 +9,8 @@ const PlantDetail = () => {
 
     return (
         <div id="plant-card">
-            <Card style={{ width: '50rem' }} className="bg-dark text-white" border="success">
-                <Card.Img src={plant.image} alt="plant" />
+            <Card style={{ width: '50rem'}} className="bg-dark text-white" border="success">
+                <Card.Img src={plant.image} style={{ maxHeight: '70vh'}} alt="plant" />
                 <Card.ImgOverlay>
                     <Card.Title style={{ fontSize: '3rem' }}>{plant.name}</Card.Title>
                 </Card.ImgOverlay>
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const routes = require('./routes');
 const db = require('./db');
 const logger = require('morgan')
 const cors = require('cors')
-// require() imports and middleware here ^ ///////
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,7 +12,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(logger('dev'))
 app.use(cors())
-// app.use() middleware here ^ ///////////////////
 
 app.use('/api', routes);
 
