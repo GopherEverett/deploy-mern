32ce52afbda4a315b00ec0baffd5abca83fc498d
John Jacobs <john.a.jacobs1973@gmail.com>
2023-02-15 09:34:10 -0500

    Deploy

--- client/src/components/PlantDetail.jsx
+++ client/src/components/PlantDetail.jsx
@@ -5,12 +5,12 @@ const PlantDetail = () => {
 
     const location = useLocation()
 
-    let {plant} = location.state
+    let { plant } = location.state
 
     return (
         <div id="plant-card">
-            <Card style={{ width: '50rem'}} className="bg-dark text-white" border="success">
-                <Card.Img src={plant.image} style={{ maxHeight: '70vh'}} alt="plant" />
+            <Card style={{ width: '50rem' }} className="bg-dark text-white" border="success">
+                <Card.Img src={plant.image} style={{ maxHeight: '70vh' }} alt="plant" />
                 <Card.ImgOverlay>
                     <Card.Title style={{ fontSize: '3rem' }}>{plant.name}</Card.Title>
                 </Card.ImgOverlay>
